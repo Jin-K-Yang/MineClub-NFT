f8cbf7591ee7138c598c72a0f1416a0ada1b18af
Jin-K-Yang <ken8989580@gmail.com>
2022-08-07 03:00:53 +0800

    complete mint function

---
+++ contracts/MineClub.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract MineClub is ERC1155Supply, Ownable {
+    using Strings for uint256;
+
+    string public baseURI;
+    mapping(uint256 => bool) public validTypeIds;
+
+    uint256 maxPerTx = 2;
+    uint256 mintPrice = 10**18;
+    uint256 maxTotalSupply = 5000;
+
+    constructor() ERC1155("") {}
+
+    /**
+     * owner only
+     */
+    function setBaseURI(string memory _baseURI) external onlyOwner {
+        baseURI = _baseURI;
+    }
+
+    function setValidTypeId(uint256 _typeId, bool _valid) external onlyOwner {
+        validTypeIds[_typeId] = _valid;
+    }
+
+    /**
+     * public
+     */
+    function mint(uint256 _id, uint256 _quantity) external payable {
+        require(valid(_id), "Valid: Not valid id");
+
+        require(
+            msg.value == _quantity * mintPrice,
+            "Purchase: Incorrect payment"
+        );
+
+        require(
+            _quantity <= maxPerTx,
+            "Purchase: Exceed the limitation of per transaction"
+        );
+
+        require(
+            totalSupply(_id) + _quantity <= maxTotalSupply,
+            "Purchase: Max total supply exceeded"
+        );
+
+        _mint(_msgSender(), _id, _quantity, "");
+    }
+
+    function uri(uint256 id) public view override returns (string memory) {
+        require(valid(id), "Valid: Not valid id");
+
+        return string(abi.encodePacked(baseURI, id.toString()));
+    }
+
+    function valid(uint256 id) public view returns (bool) {
+        return validTypeIds[id];
+    }
+}
---
+++ test/MineClub.test.js
@@ -0,0 +1,85 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("MineClub", function () {
+  let MineClub, mineclub, owner, addr1;
+  beforeEach(async function () {
+    [owner, addr1] = await ethers.getSigners();
+
+    MineClub = await ethers.getContractFactory("MineClub");
+    mineclub = await MineClub.deploy();
+    await mineclub.deployed();
+  })
+
+  describe("Deployment constructor", function () {
+    it("Should set the right owner", async function () {
+      expect(await mineclub.owner()).to.equal(owner.address);
+    })
+  })
+
+  describe("Valid Type Id", function () {
+    it("Should set the valid type id", async function () {
+      await mineclub.setValidTypeId(0, true);
+
+      expect(await mineclub.valid(0)).to.equal(true);
+      expect(await mineclub.valid(1)).to.equal(false);
+    })
+
+    it("Should be reverted because of the caller is not owner", async function () {
+      await expect(mineclub.connect(addr1).setValidTypeId(1, true)).to.be.revertedWith("Ownable: caller is not the owner");
+    })
+  })
+
+  describe("Base URI", function () {
+    const baseURI = "ipfs://test/";
+
+    it("Should set the base URI", async function () {
+      await mineclub.setValidTypeId(0, true);
+      await mineclub.setBaseURI(baseURI);
+
+      expect(await mineclub.uri(0)).to.equal(baseURI + "0");
+    })
+
+    it("Should be reverted because the caller is not owner", async function () {
+      await expect(mineclub.connect(addr1).setBaseURI(baseURI)).to.be.revertedWith("Ownable: caller is not the owner");
+    })
+
+    it("Should be reverted because id is not valid", async function () {
+      await expect(mineclub.uri(1)).to.be.revertedWith("Valid: Not valid id");
+    })
+  })
+
+  describe("Mint", function () {
+    it("Should mint VIP pass", async function () {
+      await mineclub.setValidTypeId(0, true);
+      await mineclub.mint(0, 1, { value: ethers.utils.parseEther("1.0") });
+
+      expect(await mineclub.balanceOf(owner.address, 0)).to.equal(1);
+      expect(await mineclub.totalSupply(0)).to.equal(1);
+    });
+
+    it("Should be reverted because id is not valid", async function () {
+      await expect(mineclub.mint(0, 1, { value: ethers.utils.parseEther("1.0") })).to.be.revertedWith("Valid: Not valid id");
+    })
+
+    it("Should be reverted because of the incorrect payment", async function () {
+      await mineclub.setValidTypeId(0, true);
+      await expect(mineclub.mint(0, 2, { value: ethers.utils.parseEther("1.1") })).to.be.revertedWith("Purchase: Incorrect payment");
+    });
+
+    it("Should be reverted because of the limitation of per transaction", async function () {
+      await mineclub.setValidTypeId(0, true);
+      await expect(mineclub.mint(0, 3, { value: ethers.utils.parseEther("3.0") })).to.be.revertedWith("Purchase: Exceed the limitation of per transaction");
+    })
+
+    //It will take 40 seconds to test so I comment it
+    it("Should be reverted because the limmitation of total supply", async function () {
+      await mineclub.setValidTypeId(0, true);
+      for (let i = 0; i < 2500; i++) {
+        await mineclub.mint(0, 2, { value: ethers.utils.parseEther("2.0") })
+      }
+
+      await expect(mineclub.mint(0, 1, { value: ethers.utils.parseEther("1.0") })).to.be.revertedWith("Purchase: Max total supply exceeded");
+    })
+  })
+});
